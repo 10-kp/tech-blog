27149dba995f708e49fb09d39c2738378d0c9ffc
Kp <kp_1@outlook.com>
2021-05-01 06:05:38 +0400

    Minor tweaks

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const dashboard = require('./dashboard-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
